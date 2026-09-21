87197f206a823f33b3c908383168a92f7ab9fa7b
정해왕 <j65995617@gmail.com>
2026-09-21 09:56:25 +0900

    feat: add block spawning and drop mechanics

--- app/src/main/java/com/team10/tetris/game/GameEngine.java
+++ app/src/main/java/com/team10/tetris/game/GameEngine.java
@@ -4,6 +4,7 @@ public class GameEngine {
 
     private final Board board;
     private Tetromino currentBlock;
+    private boolean gameOver = false;
 
     public GameEngine(Board board, Tetromino currentBlock) {
         this.board = board;
@@ -18,49 +19,91 @@ public class GameEngine {
         return currentBlock;
     }
 
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
     public boolean moveLeft() {
-    currentBlock.move(0, -1);
-
-    if (!board.canPlace(currentBlock)) {
-        currentBlock.move(0, 1);
-        return false;
-    }
-
-    return true;
-}
-
-public boolean moveRight() {
-    currentBlock.move(0, 1);
-
-    if (!board.canPlace(currentBlock)) {
         currentBlock.move(0, -1);
-        return false;
+
+        if (!board.canPlace(currentBlock)) {
+            currentBlock.move(0, 1);
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-}
+    public boolean moveRight() {
+        currentBlock.move(0, 1);
 
-public boolean moveDown() {
-    currentBlock.moveDown();
+        if (!board.canPlace(currentBlock)) {
+            currentBlock.move(0, -1);
+            return false;
+        }
 
-    if (!board.canPlace(currentBlock)) {
-        currentBlock.move(-1, 0);
-        board.lock(currentBlock);
-        board.clearLines();
-        return false;
+        return true;
     }
 
-    return true;
-}
+    public boolean moveDown() {
+        currentBlock.moveDown();
 
-public boolean rotateClockwise() {
-    currentBlock.rotateClockwise();
+        if (!board.canPlace(currentBlock)) {
+            currentBlock.move(-1, 0);
+            board.lock(currentBlock);
+            board.clearLines();
+            if (!spawnBlock()) {
+                gameOver = true;
+            }
+            return false;
+        }
 
-    if (!board.canPlace(currentBlock)) {
-        currentBlock.rotateCounterClockwise();
-        return false;
+        return true;
     }
 
-    return true;
-}
+    public boolean rotateClockwise() {
+        currentBlock.rotateClockwise();
+
+        if (!board.canPlace(currentBlock)) {
+            currentBlock.rotateCounterClockwise();
+            return false;
+        }
+
+        return true;
+    }
+
+    public Tetromino createRandomTetromino() {
+        TetrominoType[] types = TetrominoType.values();
+
+        int randomIndex = (int) (Math.random() * types.length);
+        TetrominoType randomType = types[randomIndex];
+
+        return new Tetromino(randomType, 0, 3);
+    }
+
+    public boolean spawnBlock() {
+        Tetromino newBlock = createRandomTetromino();
+
+        if (!board.canPlace(newBlock)) {
+            return false;
+        }
+
+        currentBlock = newBlock;
+        return true;
+    }
+
+    public void hardDrop() {
+        while (moveDown()) {
+            // 더 이상 내려갈 수 없을 때까지 반복
+        }
+    }
+
+    public void tick() {
+        if (gameOver) {
+            return;
+        }
+
+        moveDown();
+    }
+
 }
--- app/src/test/java/com/team10/tetris/game/GameEngineTest.java
+++ app/src/test/java/com/team10/tetris/game/GameEngineTest.java
@@ -27,120 +27,205 @@ class GameEngineTest {
     }
 
     @Test
-void movesBlockRight() {
-    Board board = new Board();
-    Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
-    GameEngine engine = new GameEngine(board, block);
+    void movesBlockRight() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
 
-    assertEquals(true, engine.moveRight());
-    assertEquals(5, block.getCol());
-}
-
-@Test
-void doesNotMoveBlockOutsideRightWall() {
-    Board board = new Board();
-    Tetromino block = new Tetromino(TetrominoType.T, 0, 7);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(false, engine.moveRight());
-    assertEquals(7, block.getCol());
-}
-
-@Test
-void movesBlockDown() {
-    Board board = new Board();
-    Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(true, engine.moveDown());
-    assertEquals(1, block.getRow());
-}
-
-@Test
-void doesNotMoveBlockBelowBottom() {
-    Board board = new Board();
-    Tetromino block = new Tetromino(TetrominoType.T, 18, 4);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(false, engine.moveDown());
-    assertEquals(18, block.getRow());
-}
-
-@Test
-void doesNotMoveDownWhenAnotherBlockIsBelow() {
-    Board board = new Board();
-
-    // T 블록 바로 아래에 이미 쌓인 블록이 있다고 가정
-    board.setCell(2, 5, 1);
-
-    Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(false, engine.moveDown());
-    assertEquals(0, block.getRow());
-}
-
-@Test
-void rotatesBlockClockwise() {
-    Board board = new Board();
-    Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(true, engine.rotateClockwise());
-    assertEquals(1, block.getRotation());
-}
-
-@Test
-void doesNotRotateWhenRotationCausesCollision() {
-    Board board = new Board();
-
-    // 회전된 T 블록과 겹치게 기존 블록 배치
-    board.setCell(2, 5, 1);
-
-    Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(false, engine.rotateClockwise());
-
-    // 회전이 취소되어 원래 상태로 돌아왔는지 확인
-    assertEquals(0, block.getRotation());
-}
-
-@Test
-void locksBlockWhenItCannotMoveDown() {
-    Board board = new Board();
-    Tetromino block = new Tetromino(TetrominoType.T, 18, 4);
-    GameEngine engine = new GameEngine(board, block);
-
-    assertEquals(false, engine.moveDown());
-
-    assertEquals(1, board.getCell(18, 4));
-    assertEquals(1, board.getCell(18, 5));
-    assertEquals(1, board.getCell(18, 6));
-    assertEquals(1, board.getCell(19, 5));
-}
-
-@Test
-void clearsLineAfterBlockIsLocked() {
-    Board board = new Board();
-
-    // 맨 아래 줄의 4번 칸만 제외하고 모두 채움
-    for (int col = 0; col < Board.WIDTH; col++) {
-        if (col != 4) {
-            board.setCell(19, col, 1);
-        }
+        assertEquals(true, engine.moveRight());
+        assertEquals(5, block.getCol());
     }
 
-    // I 블록을 세로로 세워 4번 칸을 채움
-    Tetromino block = new Tetromino(TetrominoType.I, 16, 4);
-    block.rotateClockwise();
+    @Test
+    void doesNotMoveBlockOutsideRightWall() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 7);
+        GameEngine engine = new GameEngine(board, block);
 
-    GameEngine engine = new GameEngine(board, block);
+        assertEquals(false, engine.moveRight());
+        assertEquals(7, block.getCol());
+    }
 
-    assertEquals(false, engine.moveDown());
+    @Test
+    void movesBlockDown() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
 
-    // 줄이 삭제됐는지 확인
-    assertEquals(0, board.getCell(19, 0));
-}
+        assertEquals(true, engine.moveDown());
+        assertEquals(1, block.getRow());
+    }
+
+    @Test
+    void doesNotMoveBlockBelowBottom() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 18, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        assertEquals(false, engine.moveDown());
+        assertEquals(18, block.getRow());
+    }
+
+    @Test
+    void doesNotMoveDownWhenAnotherBlockIsBelow() {
+        Board board = new Board();
+
+        // T 블록 바로 아래에 이미 쌓인 블록이 있다고 가정
+        board.setCell(2, 5, 1);
+
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        assertEquals(false, engine.moveDown());
+        assertEquals(0, block.getRow());
+    }
+
+    @Test
+    void rotatesBlockClockwise() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        assertEquals(true, engine.rotateClockwise());
+        assertEquals(1, block.getRotation());
+    }
+
+    @Test
+    void doesNotRotateWhenRotationCausesCollision() {
+        Board board = new Board();
+
+        // 회전된 T 블록과 겹치게 기존 블록 배치
+        board.setCell(2, 5, 1);
+
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        assertEquals(false, engine.rotateClockwise());
+
+        // 회전이 취소되어 원래 상태로 돌아왔는지 확인
+        assertEquals(0, block.getRotation());
+    }
+
+    @Test
+    void locksBlockWhenItCannotMoveDown() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 18, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        assertEquals(false, engine.moveDown());
+
+        assertEquals(1, board.getCell(18, 4));
+        assertEquals(1, board.getCell(18, 5));
+        assertEquals(1, board.getCell(18, 6));
+        assertEquals(1, board.getCell(19, 5));
+    }
+
+    @Test
+    void clearsLineAfterBlockIsLocked() {
+        Board board = new Board();
+
+        // 맨 아래 줄의 4번 칸만 제외하고 모두 채움
+        for (int col = 0; col < Board.WIDTH; col++) {
+            if (col != 4) {
+                board.setCell(19, col, 1);
+            }
+        }
+
+        // I 블록을 세로로 세워 4번 칸을 채움
+        Tetromino block = new Tetromino(TetrominoType.I, 16, 4);
+        block.rotateClockwise();
+
+        GameEngine engine = new GameEngine(board, block);
+
+        assertEquals(false, engine.moveDown());
+
+        // 줄이 삭제됐는지 확인
+        assertEquals(0, board.getCell(19, 0));
+    }
+
+    @Test
+    void spawnsNewBlock() {
+        Board board = new Board();
+        Tetromino initialBlock = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, initialBlock);
+
+        assertEquals(true, engine.spawnBlock());
+
+        Tetromino spawnedBlock = engine.getCurrentBlock();
+
+        assertEquals(0, spawnedBlock.getRow());
+        assertEquals(3, spawnedBlock.getCol());
+    }
+
+    @Test
+    void spawnedBlockHasValidType() {
+        Board board = new Board();
+        Tetromino initialBlock = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, initialBlock);
+
+        engine.spawnBlock();
+
+        TetrominoType type = engine.getCurrentBlock().getType();
+
+        boolean validType = false;
+
+        for (TetrominoType value : TetrominoType.values()) {
+            if (value == type) {
+                validType = true;
+                break;
+            }
+        }
+
+        assertEquals(true, validType);
+    }
+
+    @Test
+    void gameOverWhenNewBlockCannotSpawn() {
+        Board board = new Board();
+
+        // 새 블록 생성 위치를 막되, 완성된 줄이 되지 않도록 일부 칸만 채움
+        for (int col = 3; col <= 6; col++) {
+            board.setCell(0, col, 1);
+            board.setCell(1, col, 1);
+        }
+
+        Tetromino block = new Tetromino(TetrominoType.T, 18, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        // 현재 블록 고정 → 다음 블록 생성 시도
+        engine.moveDown();
+
+        assertEquals(true, engine.isGameOver());
+    }
+
+    @Test
+    void hardDropsBlockToBottom() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        engine.hardDrop();
+
+        // T 블록이 바닥까지 내려가 고정되었는지 확인
+        assertEquals(1, board.getCell(18, 4));
+        assertEquals(1, board.getCell(18, 5));
+        assertEquals(1, board.getCell(18, 6));
+        assertEquals(1, board.getCell(19, 5));
+    }
+
+    @Test
+    void movesBlockDownOnTick() {
+        Board board = new Board();
+        Tetromino block = new Tetromino(TetrominoType.T, 0, 4);
+        GameEngine engine = new GameEngine(board, block);
+
+        engine.tick();
+
+        assertEquals(1, block.getRow());
+
+        engine.tick();
+
+        assertEquals(2, block.getRow());
+    }
 
 }
